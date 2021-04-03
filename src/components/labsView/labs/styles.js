4646a45995903d06bce3110847c958016e872572
Michael Jones <mjones@kotrs.com>
2021-04-02 20:54:19 -0400

    100% width to grid

--- src/components/labsView/labs/styles.js
+++ src/components/labsView/labs/styles.js
@@ -23,6 +23,7 @@ export const Grid = styled.div`
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: 8fr;
   gap: 1.2rem 1.2rem;
+  width: 100%;
 
   @media (max-width: 960px) {
     grid-template-columns: repeat(2, 1fr);
